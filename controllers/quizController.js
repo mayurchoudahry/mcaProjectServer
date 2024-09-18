68ac69e80b98e1a155eadc467c578f4f5001bc2c
mayurchoudahry <mjchoudhary2337@gmail.com>
2024-09-18 12:56:15 +0530

    difficultuy

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -5,19 +5,19 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 exports.generateQuiz = async (req, res) => {
-  const { topic, questionType } = req.body;
+  const { topic, questionType , difficulty } = req.body;
 
   if (!topic || !questionType) {
     return res.status(400).json({ message: 'Topic and questionType are required.' });
   }
 
-  let prompt = `Generate 30 ${questionType} questions on the topic of "${topic}". Each question should have 4 options and one correct answer and give the answer key always. Don't generate anything else except question, option, and answer. Generate this type of response: '**1. What is the capital of France?',
+  let prompt = `Generate 30 ${questionType} questions on the topic of "${topic}" And difficulty should be ${difficulty}. Each question should have 4 options and one correct answer and give the answer key always. Don't generate anything else except question, option, and answer. Generate this type of response: '**1. What is the capital of France?',
   '    1) Paris',
   '    2) London',
   '    3) Rome',
   '    4) Berlin',
   '    Answer: Paris',`;
-
+  
   if (questionType === 'MCQ') {
     prompt += ' Each question should have 4 options and one correct answer.';
   } else if (questionType === 'Short Answer') {
